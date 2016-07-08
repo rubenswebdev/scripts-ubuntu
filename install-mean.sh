00900b5509086dd84f7a109afdaa70c1f1277e67
Rubens Fernandes <rubensdrk@gmail.com>
2016-07-07 22:27:00 -0300

    set permissions

--- install-mean.sh
+++ install-mean.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install unzip
-sudo apt-get install apache2
-sudo apt-get install libapache2-mod-proxy-html libxml2-dev
+sudo apt-get install -y unzip
+sudo apt-get install -y apache2
+sudo apt-get install -y libapache2-mod-proxy-html libxml2-dev
 sudo a2enmod proxy proxy_http proxy_ajp rewrite deflate headers proxy_balancer proxy_connect proxy_html
-sudo apt-get install curl
+sudo apt-get install -y curl
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm install -g pm2
@@ -16,10 +16,9 @@ sudo apt-get install -y mongodb-org
 sudo npm install grunt-cli -g
 git clone https://github.com/rubensfernandes/vhost-manager.git
 cd vhost-manager
-sudo chmod +x vhost.bash
-sudo ./vhost.bash -install
+sudo chmod +x vhost
+sudo ./vhost -install
 sudo apt-add-repository ppa:fish-shell/release-2
 sudo apt-get update
-sudo apt-get install fish
+sudo apt-get install -y fish
 chsh -s /usr/bin/fish
-sudo apt-get install mercurial
