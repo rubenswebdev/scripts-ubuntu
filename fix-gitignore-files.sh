700cd319870c7e9389f372859a622bc98453afee
Rubens Fernandes <rubensdrk@gmail.com>
2016-07-21 09:51:25 -0300

    removido scripts nao utilizados

--- fix-gitignore-files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ "$1" != "" ]; then
-	cd "$1"
-    git rm -r --cached .
-	git add .
-	git commit -m "fixed untracked files"
-else
-    echo "Digite o path do projeto"
-fi
--- npm_without_root.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo chown -R $(whoami) ~/.npm
